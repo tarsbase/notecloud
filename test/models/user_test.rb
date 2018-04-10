563c6a5cfd4b84b11f65131843b7a925dbbc6109
Alex <alexanderhapp@me.com>
2018-04-10 11:42:38 -0700

    User model validations and auth business logic

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
