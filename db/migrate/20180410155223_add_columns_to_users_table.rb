4dfae7961f192e85463d18ce51e29a8cb9460d5a
Alex <alexanderhapp@me.com>
2018-04-10 08:56:06 -0700

    generate migration for users table

---
+++ db/migrate/20180410155223_add_columns_to_users_table.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUsersTable < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :username, :string, null: false
+    add_column :users, :password_digest, :string, null: false
+    add_column :users, :session_token, :string, null: false
+    ad_index :users, :username, :unique: true
+  end
+end
