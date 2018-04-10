ddaeb8ce7376ffa3b3f7d469f6e191a8f26f8469
Alex <alexanderhapp@me.com>
2018-04-10 12:06:12 -0700

    Add validations and associations for sessions

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: sessions
+#
+#  id            :integer          not null, primary key
+#  session_token :string           not null
+#  user_id       :integer          not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Session < ApplicationRecord
+  validates :session_token, :user_is, presence: true
+
+  belongs_to :user
 end
