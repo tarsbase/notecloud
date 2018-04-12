f6c17c142e49f35c7d83e37845863a3186616bb3
Alex <alexanderhapp@me.com>
2018-04-12 10:25:31 -0700

    Create main nav component

---
+++ frontend/components/main/main_nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default class MainNav extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <nav>
+        <h1>NOTECLOUD</h1>
+      </nav>
+    );
+  }
+}
