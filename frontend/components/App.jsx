e36f450240f7a956d189644fa44ab1d96783a4c2
Alex <alexanderhapp@me.com>
2018-04-11 18:02:51 -0700

    Add App and root components

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Evernote!</h1>
+  </div>
+);
+
+export default App;
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {login, signup, logout} from './util/session_api_util';
+import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout;
-  
+  const store = configureStore();
+
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Hello!</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
