abe48eede3776fa6e9e4d22d956d3a8facb62feb
Alex <alexanderhapp@me.com>
2018-04-16 13:41:06 -0700

    Wrote tests for note model

---
+++ spec/factories/api_users.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :api_user, class: 'Api::User' do
+    
+  end
+end
--- spec/models/api/note_spec.rb
+++ spec/models/api/note_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Api::Note, type: :model do
     expect(FactoryBot.build(:note)).to be_valid
   end 
 
+  context 'is invalid' do 
+    specify 'when title is blank' do
+      expect(FactoryBot.build(:note, title: '')).not_to be_valid
+    end
+
+    specify 'when body is blank' do
+      expect(FactoryBot.build(:note, body: '')).not_to be_valid
+    end 
+
+    specify 'when notebook is blank' do 
+      expect(FactoryBot.build(:note, notebook: nil)).not_to be_valid
+    end 
+  end 
 end
