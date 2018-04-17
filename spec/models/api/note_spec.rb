16a4d4d14a0d86502681d68fa5ae1752f993f9d0
Alex <alexanderhapp@me.com>
2018-04-16 17:57:07 -0700

    Refactor note spec with shoulda matchers

--- spec/models/api/note_spec.rb
+++ spec/models/api/note_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Api::Note, type: :model do
-  
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should belong_to(:notebook) }
 end
