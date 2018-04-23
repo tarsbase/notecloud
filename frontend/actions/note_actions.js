fb56d51ea1420623b5433bac71f38c3e0df35438
Alex <alexanderhapp@me.com>
2018-04-23 14:58:50 -0700

    Add note actions

--- frontend/actions/note_actions.js
+++ frontend/actions/note_actions.js
@@ -3,3 +3,26 @@ import * as NoteApiUtil from '../util/note_api_util';
 export const RECEIVE_NOTE = 'RECEIVE_NOTE';
 export const RECEIVE_ALL_NOTES = 'RECEIVE_ALL_NOTES';
 export const REMOVE_NOTE = 'REMOVE_NOTE';
+
+const receiveNote = note => ({
+  type: RECEIVE_NOTE,
+  note
+});
+
+const receiveAllNotes = notes => ({
+  type: RECEIVE_ALL_NOTES,
+  notes
+});
+
+const removeNote = note => ({
+  type: REMOVE_NOTE,
+  note
+});
+
+export const getAllNotes = () => dispatch =>
+  NoteApiUtil.fetchAllNotes().then(notes => dispatch(receiveAllNotes(notes)));
+
+export const getNote = id => dispatch =>
+  NoteApiUtil.fetchNote(id).then(note => dispatch(receiveNote(note)));
+
+
