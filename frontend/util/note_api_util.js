cc5734888bfed5b725ad3a20dafd3d396df7539f
Alex <alexanderhapp@me.com>
2018-04-23 14:50:04 -0700

    Write post and patch ajax methods for notes

--- frontend/util/note_api_util.js
+++ frontend/util/note_api_util.js
@@ -5,9 +5,32 @@ export const fetchAllNotes = () => (
   })
 );
 
-export const fetchNote = (id) => (
+export const fetchNote = id => (
   $.ajax({
     method: 'GET',
-    url: `/api/notes/${id}`
+    url: `api/notes/${id}`
+  })
+);
+
+export const fetchNotesByNotebook = notebookId => (
+  $.ajax({
+    method: 'GET',
+    url: `api/notebooks/${notebookId}/notes`
+  })
+);
+
+export const createNote = note => (
+  $.ajax({
+    method: 'POST',
+    url: `api/notes`,
+    data: { note }
+  })
+);
+
+export const updateNote = note => (
+  $.ajax({
+    method: 'PATCH',
+    url: `api/notes/${note.id}`,
+    data: { note }
   })
 );
