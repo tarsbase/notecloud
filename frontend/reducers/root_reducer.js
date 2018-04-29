6d4f2ccee5477a05826533216b179f5d5a393aaa
Alex <alexanderhapp@me.com>
2018-04-29 13:42:59 -0700

    Add ui to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
 import NotesReducer from './notes_reducer';
+import UIReducer from './ui_reducer';
 
 const rootReducer = combineReducers({
   session: SessionReducer,
   errors: errorsReducer,
-  notes: NotesReducer
+  notes: NotesReducer,
+  ui: UIReducer
 });
 
 export default rootReducer;
