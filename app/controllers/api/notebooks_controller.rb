590feb81f7ee4e36641a95227959983f528a2909
Alex <alexanderhapp@me.com>
2018-05-04 14:12:13 -0700

    Write notebook controller methods

--- app/controllers/api/notebooks_controller.rb
+++ app/controllers/api/notebooks_controller.rb
@@ -1,18 +1,44 @@
 class Api::NotebooksController < ApplicationController
   def create
-    
+    @notebook = Notebook.new(notebook_params)
+    @notebook.user_id = current_user.id
+
+    if @notebook.save! 
+      render :show
+    else 
+      render json: @notebook.errors.full_messages, status: 422
+    end 
   end
 
   def show 
+    @notebook = current_user.notebooks.find(params[:id]).includes(:notes)
 
+    if @notebook 
+      render :show
+    else 
+      render json: ["This notebook belongs to a different user"], status: 404
+    end 
   end 
 
   def index 
+    @notebooks = current_user.notebooks
+    render :index
+  end 
 
+  def update 
+    @notebook = current_user.notebooks.find(params[:id])
+
+    if @notebook.update_attributes(notebook_params)
+      render :show
+    else
+      render json: @notebook.errors.full_messages, status: 422
+    end 
   end 
 
   def destroy
-
+    @notebook = current_user.notebooks.find(params[:id])
+    @notebook.destroy! 
+    render :show
   end 
 
   private 
