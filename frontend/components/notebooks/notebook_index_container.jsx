34f231455b00cd201a3f593cf467ff7a4c1aa9e7
Alex <alexanderhapp@me.com>
2018-05-04 11:57:19 -0700

    Wrote notebook api utils

--- frontend/components/notebooks/notebook_index_container.jsx
+++ frontend/components/notebooks/notebook_index_container.jsx
@@ -1,2 +1,15 @@
 import { connect } from 'react-redux';
 import NotebookIndex from './notebook_index';
+
+const mapStateToProps = (state, ownProps) => ({
+  notebooks: Object.values(state.notebooks)
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NotebookIndex);
---
+++ frontend/util/noptebook_api_util.js
@@ -0,0 +1,36 @@
+export const fetchAllNotebooks = () => (
+  $.ajax({
+    method: 'GET',
+    url: `api/notebooks`
+  })
+);
+
+export const fetchNotebook = id => (
+  $.ajax({
+    method: 'GET',
+    url: `api/notebooks/${id}`
+  })
+);
+
+export const createNotebook = notebook => (
+  $.ajax({
+    method: 'POST',
+    url: `api/notebooks`,
+    data: { notebook }
+  })
+);
+
+export const updateNotebook = notebook => (
+  $.ajax({
+    method: 'PATCH',
+    url: `api/notebooks/${notebook.id}`,
+    data: { notebook }
+  })
+);
+
+export const destroyNotebook = id => (
+  $.ajax({
+    method: 'DELETE',
+    url: `api/notebooks/${id}`
+  })
+);
