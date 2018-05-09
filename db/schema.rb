f2bb4c40af050d1302cb562a6bc7cee91947a072
Alex <alexanderhapp@me.com>
2018-05-09 16:11:32 -0700

    Add taggings model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180509222506) do
+ActiveRecord::Schema.define(version: 20180509230905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 20180509222506) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "note_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tag_id", "note_id"], name: "index_taggings_on_tag_id_and_note_id", unique: true
   end
 
   create_table "tags", force: :cascade do |t|
---
+++ spec/factories/taggings.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :tagging do
+    tag_id 1
+    note_id 1
+  end
+end
