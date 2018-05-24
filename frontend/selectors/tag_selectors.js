799bad3133fd694e20956c3cc2767c61a84e6557
Alex <alexanderhapp@me.com>
2018-05-23 22:05:31 -0700

    Write getSortedTags selector

--- frontend/selectors/tag_selectors.js
+++ frontend/selectors/tag_selectors.js
@@ -12,5 +12,16 @@ export const getTagObject = state => {
 };
 
 export const getSortedTags = state => {
-  return Object.values(state.tags).sort((a,b) => a.name - b.name);
+  return Object.values(state.tags).sort((a,b) => {
+    let nameA = a.name.toUpperCase();
+    let nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    return 0;
+  });
 };
