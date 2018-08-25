bdc4f5b8a9077c44959d90ac9a736951145e4a8b
Alex <alexanderhapp@me.com>
2018-08-24 20:29:58 -0700

    Fix typo

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 
-var plugins = []; // if using any plugins for both dev and production
+var plugins = []; // if using any plugins for both dev and productioon
 var devPlugins = []; // if using any plugins for development
 
 var prodPlugins = [
